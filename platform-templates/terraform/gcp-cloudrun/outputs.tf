e70fbb237b2e9b1a7a364f7a8dc2a96884d51086
Asikahamed <96490250+Asikahamed@users.noreply.github.com>
2026-07-02 01:37:18 +0530

    Add descriptions to Cloud Run output variables
    Added descriptions for service_name and service_url outputs.

--- platform-templates/terraform/gcp-cloudrun/outputs.tf
+++ platform-templates/terraform/gcp-cloudrun/outputs.tf
@@ -1,10 +1,14 @@
 output "service_name" {
 
+  description = "Cloud Run Service Name"
+
   value = google_cloud_run_v2_service.app.name
 
 }
 
 output "service_url" {
+
+  description = "Cloud Run URL"
 
   value = google_cloud_run_v2_service.app.uri
 
